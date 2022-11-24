12c46fb6596069af5272dca348a478e6490efba6
alireza <a.jalilian66@gmail.com>
2022-11-25 02:16:19 +0330

    add => Further Input and Output Styling and Configuration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,18 @@ class NewTransaction extends StatelessWidget {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController amountController = TextEditingController();
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <=0){
+      return;
+    }
+    addTx(
+        enteredTitle,
+        enteredAmount
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -21,20 +33,18 @@ class NewTransaction extends StatelessWidget {
                   labelText: 'Title'
               ),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(
                   labelText: 'Amount'
               ),
               controller:  amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
             ),
             ElevatedButton(
-              onPressed: () {
-                addTx(
-                    titleController.text,
-                    double.parse(amountController.text)
-                );
-              },
+              onPressed: submitData,
               child: Text('Add Transaction'),
             )
           ],
